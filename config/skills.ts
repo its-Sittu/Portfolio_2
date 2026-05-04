f39343b1b93d5bd9abb800aa312bcc1db6566b9d
unknown <sitturaj730@gmail.com>
2026-05-04 23:33:10 +0530

    feat: add Cloudflare to skills ecosystem

--- config/skills.ts
+++ config/skills.ts
@@ -80,4 +80,5 @@ export const skillCategories: SkillCategory[] = [
             { name: "Heroku", icon: "code" },
             { name: "DigitalOcean", icon: "code" },
             { name: "Linode", icon: "code" },
+            { name: "Cloudflare", icon: "code" },
 ]
